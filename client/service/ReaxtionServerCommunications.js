ba80f8a4273b12b97ed0008f3e3611e3f4c2eb98
Parveen Lukha <Parveen.lukha@daffodilsw.com>
2015-09-05 12:55:52 +0530

    Updated ReaxtionServerCommunications.js

--- client/service/ReaxtionServerCommunications.js
+++ client/service/ReaxtionServerCommunications.js
@@ -15,13 +15,145 @@ reaxtion.service('ReaxtionServerCommunicationsService', function ($http, ngDialo
 
                                    
                     callback(null, response);
-                }
-
-            }).
+                }).
             error(function (data, status, headers, config) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
             });
-
+     this.addProgrammingLanguages = function(callback) {
+        var method="post";
+        var insertUrl="http://localhost:3000/language/add_language";
+            var language1={
+                "l_id":"L1",
+                "lname":"C",
+                "videos":[
+                    {
+                        "video_id":"video1"
+                    },
+                    {
+                        "video_id":"video2"
+                    },
+                    {
+                        "video_id":"video3"
+                    }
+                ]
+            };
+            var language2={
+                "l_id":"L2",
+                "lname":"C++",
+                "videos":[
+                    {
+                        "video_id":"video1"
+                    },
+                    {
+                        "video_id":"video2"
+                    },
+                    {
+                        "video_id":"video3"
+                    }
+                ]
+            };
+            var language3={
+                "l_id":"L3",
+                "lname":"C#",
+                "videos":[
+                    {
+                        "video_id":"video1"
+                    },
+                    {
+                        "video_id":"video2"
+                    },
+                    {
+                        "video_id":"video3"
+                    }
+                ]
+            };
+            var language4={
+                "l_id":"L4",
+                "lname":"Java",
+                "videos":[
+                    {
+                        "video_id":"video1"
+                    },
+                    {
+                        "video_id":"video2"
+                    },
+                    {
+                        "video_id":"video3"
+                    }
+                ]
+            };
+            var language5={
+                "l_id":"L5",
+                "lname":"Javascript",
+                "videos":[
+                    {
+                        "video_id":"video1"
+                    },
+                    {
+                        "video_id":"video2"
+                    },
+                    {
+                        "video_id":"video3"
+                    }
+                ]
+            };
+            
+            var jsonL1=JSON.stringify(language1);
+            var jsonL2=JSON.stringify(language2);
+            var jsonL3=JSON.stringify(language3);
+            var jsonL4=JSON.stringify(language4);
+            var jsonL5=JSON.stringify(language5);
+            $http({
+                method:method,
+                url:insertUrl,
+                data:jsonL1,
+            }).success(function(response){
+                console.log(response);
+            }).error(function(response){
+                console.log("error");
+                console.log(response);
+            });
+            $http({
+                method:method,
+                url:insertUrl,
+                data:jsonL2,
+            }).success(function(response){
+                console.log(response);
+            }).error(function(response){
+                console.log("error");
+                console.log(response);
+            });
+            $http({
+                method:method,
+                url:insertUrl,
+                data:jsonL3,
+            }).success(function(response){
+                console.log(response);
+            }).error(function(response){
+                console.log("error");
+                console.log(response);
+            });
+            $http({
+                method:method,
+                url:insertUrl,
+                data:jsonL4,
+            }).success(function(response){
+                console.log(response);
+            }).error(function(response){
+                console.log("error");
+                console.log(response);
+            });
+            $http({
+                method:method,
+                url:insertUrl,
+                data:jsonL5,
+            }).success(function(response){
+                console.log(response);
+            }).error(function(response){
+                console.log("error");
+                console.log(response);
+            });
+     }   
     }
 });
