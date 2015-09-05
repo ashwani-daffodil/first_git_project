9f38bb8bbd6d5d465b3c66e96e7025190548f5b8
baljeet-daffodil <baljeet.verma@daffodilsw.com>
2015-09-05 17:26:13 +0530

    improved css

--- client/view/dashboard/dashboard.js
+++ client/view/dashboard/dashboard.js
@@ -6,12 +6,13 @@
  * Created by Praveen on 24-02-2015.
  */
 
-reaxtion.controller('dashboardViewController', ['$scope', 'ServerCommunicationsService', 'SystemService', function ($scope, ServerCommunicationsService) {
+reaxtion.controller('dashboardViewController', ['$scope', 'ServerCommunicationsService', '$sce', function ($scope, ServerCommunicationsService, $sce) {
 
 
     $scope.programmingLanguages = [];
     $scope.videos = [];
-
+    $scope.selectedVideo = {};
+    $scope.selectedProgrammingLanguage = {};
 
     //ServerCommunicationsService.addProgrammingLanguages(function() {
 //
@@ -34,5 +35,17 @@ reaxtion.controller('dashboardViewController', ['$scope', 'ServerCommunicationsS
             $scope.programmingLanguages = response;
         }
       });
+
+    $scope.selectVideo = function(video) {
+        $scope.selectedVideo = video;
+    }
+
+    $scope.getTrustedVideoUrl = function() {
+        return $sce.trustAsResourceUrl($scope.selectedVideo.video_desc[0].video_url);
+    }
+
+    $scope.selectProgrammingLanguage = function(programmingLanguage) {
+        $scope.selectedProgrammingLanguage = programmingLanguage;
+    }
 
 }]);
