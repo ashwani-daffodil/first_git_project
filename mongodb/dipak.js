e898d161110355430b7db928da4493f7c940edc5
rahul-daffodil <rahul.kamboj@daffodilsw.com>
2015-09-24 19:06:36 +0530

    ok

--- mongodb/dipak.js
+++ mongodb/dipak.js
@@ -84,3 +84,83 @@ describe('add()', function () {
             }
         ],
         service: {
+
+
+escribe('add()', function () {
+	var tests = [
+		{args : [1, 2], expected : 3},
+		{args : [1, 2, 3], expected : 7},
+		{args : [1, 2, 3, 4], expected : 10}
+	];
+	tests.forEach( function (test) {
+	it('correctly adds ' + test.args.length + 'args', function () {
+		var res = add.apply( null , test.args);
+		assert.equal(res, test.expected);
+		});
+	});
+});     {
+                        label: "Applane User", field: "user_id.username", ui: "text",span:2
+                    },
+                    {
+                        label: "Joining Date", field: "joining_date", ui: "date",span:2
+                    },
+                    {
+                        label: "Unit", field: "actual_profit_center_id.name", ui: "text",span:2
+                    },
+                    {
+                        label: "Sitting Location", field: "sitting_location_id.name", ui: "text",span:2
+                    },
+                    {
+                        label: "Orgnization", field: "org_id.name", ui: "text",span:2
+                    }
+
+
+
+                ]
+            }
+        ],
+        service: {
+
+
+var assert = require('assert');
+function add() {
+	return Array.prototype.slice.call(arguments).reduce( function (prev, curr) {
+
+		return prev + curr;
+	}, 0);
+}
+
+describe('add()', function () {
+	var tests = [
+		{args : [1, 2], expected : 3},
+		{args : [1, 2, 3], expected : 7},
+		{args : [1, 2, 3, 4], expected : 10}
+	];
+	tests.forEach( function (test) {
+	it('correctly adds ' + test.args.length + 'args', function () {
+		var res = add.apply( null , test.args);
+		assert.equal(res, test.expected);
+		});
+	});
+});     {
+                        label: "Applane User", field: "user_id.username", ui: "text",span:2
+                    },
+                    {
+                        label: "Joining Date", field: "joining_date", ui: "date",span:2
+                    },
+                    {
+                        label: "Unit", field: "actual_profit_center_id.name", ui: "text",span:2
+                    },
+                    {
+                        label: "Sitting Location", field: "sitting_location_id.name", ui: "text",span:2
+                    },
+                    {
+                        label: "Orgnization", field: "org_id.name", ui: "text",span:2
+                    }
+
+
+
+                ]
+            }
+        ],
+        service: {
